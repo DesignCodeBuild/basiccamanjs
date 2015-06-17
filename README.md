bcfd06aee23da6a9e34a9ac552409b3c75a2c626
David Berard <davidberard98@gmail.com>
2015-06-16 23:24:46 -0700

    TEST.php makes thumbnails using wordpress functions, (does not write to database yet).

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Important notes
 + Use the NEWEST release of CamanJS: the one from github.  Because 4.1.1 and the CDN version have a bug in Caman.revert(true)
 + Must transfer image from CamanJS to php through AJAX, and must reconfigure base64 because it contains '/', ';', ':', '+', '/'
++ wordpress/wp-admin/TEST.php will currently make thumbnails for specific images.
+  - This line: wp-content/themes/twentythirteen/functions.php: set\_post\_thumbnail\_size( 604, 270, true );
+  - It is difficult because there is a specific size (in this case, 604, 270) that is determined by the theme, where the url will vary.
+  - Need to figure out how to find ID for an image once submitted.
---
+++ wordpress/wp-admin/TEST.php
@@ -0,0 +1,92 @@
+<?
+require_once( dirname( __FILE__ ) . '/admin.php' );
+require_once( '../wp-includes/post.php' );
+require_once( '../wp-includes/media.php' );
+require_once( '../wp-includes/option.php' );
+require_once( '../wp-content/themes/twentythirteen/functions.php' );
+$postnine = get_post(9);
+$orig_content=$postnine->post_content;
+$postnine->post_content="[gallery ids=\"11,12,13,14,16,21\"]\n\nabc";
+//wp_update_post($postnine);
+
+//////////
+//$imageLocation=ABSPATH ."/wordpress/wp-content/uploads/2015/06/631px-Barenmutter_Junges_3_2004-11-17.jpg";
+$imageLocation="../wp-content/uploads/2015/06/631px-Barenmutter_Junges_3_2004-11-17.jpg";
+$imageDirectory="../wp-content/uploads/2015/06/";
+$imageName = "631px-Barenmutter_Junges_3_2004-11-17"; //without extention
+$imageExtension = "jpg";
+$imageDescription="A polar bear resides in a Canadian national park";
+$imageCaption="A Polar Bear";
+$imageMimeType="image/jpeg";
+//////////
+
+$iarray;
+
+function ce_get_image_sizes()
+{
+  $ce_sizes = array();
+  foreach(array('thumbnail', 'medium') as $ce_size)
+  {
+    $ce_sizes[ $ce_size ]['width'] = get_option( $ce_size . '_size_w' );
+    $ce_sizes[ $ce_size ]['height'] = get_option( $ce_size . '_size_h' );
+    $ce_sizes[ $ce_size ]['crop'] = get_option( $ce_size . '_crop' );
+  }
+  $ce_sizes['sixzerofour'] = array('width' => 604, 'height' => 270, 'crop'=>true);
+  return $ce_sizes;
+}
+
+$requiredSizes = ce_get_image_sizes();
+
+$image = wp_get_image_editor($imageLocation);
+if( ! is_wp_error($image) )
+{
+  $iarray = $image->multi_resize($requiredSizes);
+}
+else
+{
+  echo "error";
+}
+
+$files = array ($iarray['thumbnail']['width'], $iarray['medium']['file'], $iarray['large']['file']);
+//$files = array("a","b","c");
+
+//before adding to database, must first:
+//save in ./wp-content/uploads/yyyy/mm/
+//save copies in 150x150, 300x___, 604x___
+
+
+$UploadPicture = array(
+//'ID'		=> {}, // leave empty to specify that this is a NEW
+  'post_content'=> $imageDescription,
+  'post_name' => strtolower($imageName),
+  'post_title' => $imageName,
+  'post_status' => 'inherit',
+  'post_type' => 'attachment',
+  'post_author'	=> 1,
+  'post_excerpt'	=> $imageCaption,
+  'post_date'	=> date('Y-m-d H:i:s'),
+  'post_date_gmt'	=> date('Y-m-d H:i:s'),
+  'post_mime_type'	=> $imageMimeType
+);
+
+?>
+
+
+<html>
+<head><title>HACKING INTO WORDPRESS</title>
+<script type="text/javascript">
+</script>
+<style>
+body
+{
+background-color:black;
+color:white;
+}
+</style>
+</head>
+<body>
+Here we are <br /> <br />
+
+<textarea id="phpstuff"><?php echo $files[0] . "\n" . $files[1] . "\n" . $files[2]; ?></textarea>
+</body>
+</html>
